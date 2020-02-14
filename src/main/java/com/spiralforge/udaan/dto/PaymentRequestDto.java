34e8af4ad3e175f934177095b30903b005fee663
Sri-Keerthna <srikeerthi2209@gmail.com>
2020-02-14 15:23:48 +0530

    Merging api for sprint 1 demo

--- src/main/java/com/spiralforge/udaan/dto/PaymentRequestDto.java
+++ src/main/java/com/spiralforge/udaan/dto/PaymentRequestDto.java
@@ -1,5 +1,8 @@
 package com.spiralforge.udaan.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,10 +14,13 @@ import lombok.Setter;
 @Setter
 public class PaymentRequestDto {
 
+	
 	private Long schemeId;
 	private String userName;
+	@Email(message = "Email should be valid")
 	private String emailId;
 	private Long mobileNumber;
+	@Size(min = 10, max = 10)
 	private String panNumber;
 	
 }
